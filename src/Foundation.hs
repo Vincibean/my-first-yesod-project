eef73a6f3a2ad5cbfb584c294784097a650c1955
Vincibean <andrebessi00@gmail.com>
2019-10-02 19:20:00 +0100

    Fixed compile time warning

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -159,17 +159,11 @@ instance Yesod App where
         :: Route App  -- ^ The route the user is visiting.
         -> Bool       -- ^ Whether or not this is a "write" request.
         -> Handler AuthResult
-    -- Routes not requiring authentication.
-    isAuthorized (AuthR _) _ = return Authorized
-    isAuthorized HomeR _ = return Authorized
-    isAuthorized HelloR _ = return Authorized
-    isAuthorized FaviconR _ = return Authorized
-    isAuthorized RobotsR _ = return Authorized
-    isAuthorized (StaticR _) _ = return Authorized
-
     -- the profile route requires that the user is authenticated, so we
     -- delegate to that function
-    isAuthorized ProfileR _ = isAuthenticated
+    isAuthorized ProfileR _ = isAuthenticated        
+    -- Routes not requiring authentication.
+    isAuthorized _ _ = return Authorized
 
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
