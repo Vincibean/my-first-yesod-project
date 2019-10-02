6a2151c9797f0dc83119d601bc499d8f6e163f00
Vincibean <andrebessi00@gmail.com>
2019-10-02 21:23:14 +0100

    forms for day 2

---
+++ src/Handler/Users.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes           #-}
+module Handler.Users where
+
+import           Import
+
+getUsersR :: Handler Html
+getUsersR = do
+    users                     <- runDB $ selectList [] [Asc UserIdent]
+    (userFormWidget, enctype) <- generateFormPost userForm
+    defaultLayout $(widgetFile "users")
+
+postUsersR :: Handler Html
+postUsersR = do
+    ((result, _), _) <- runFormPost userForm
+    case result of
+        FormSuccess user -> do 
+            _ <- runDB $ insert user
+            redirect UsersR
+        _  -> defaultLayout [whamlet|whoops|]
+
+userForm :: Form User
+userForm =
+    renderDivs
+        $   User
+        <$> areq textField "ID" Nothing
+        <*> aopt textField "Password" Nothing
