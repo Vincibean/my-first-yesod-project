368a8dcecd4e8a74eeb24935419c7c8adf7cd235
Vincibean <andrebessi00@gmail.com>
2019-10-05 23:24:56 +0100

    end of day 2

--- src/Handler/Greet3.hs
+++ src/Handler/Greet3.hs
@@ -5,6 +5,8 @@ module Handler.Greet3 where
 
 import           Import
 
-getGreet3R :: Text -> Handler Html
-getGreet3R name = defaultLayout $(widgetFile "greet3")
-    where color = "blue" :: Text
+getGreet3R :: Handler Html
+getGreet3R = do
+    name <- newIdent
+    defaultLayout $(widgetFile "greet3")
+     where color = "blue" :: Text
