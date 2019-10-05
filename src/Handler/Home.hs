3efa018b2b3ad8b1e54b0e7632714a16c325796f
Vincibean <andrebessi00@gmail.com>
2019-10-05 19:12:09 +0100

    clean code

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,7 +7,6 @@ module Handler.Home where
 
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
